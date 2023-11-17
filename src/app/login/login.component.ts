a61d9a3446506d2da05cde0ebf802beb7b8bf2d6
celsotavares <celsotavares@pm.me>
2023-11-17 08:47:52 -0300

    refactor: authentication.guard to auth.service

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { Login } from '../models/login';
 import { Register } from '../models/register';
 import { JwtAuth } from '../models/jwtAuth';
-import { AuthenticationService } from '../services/authentication.service';
+import { AuthService } from '../services/auth.service';
 import { Router } from '@angular/router';
 import { catchError, tap, throwError } from 'rxjs';
 import { ToastrService } from 'ngx-toastr';
@@ -20,48 +20,50 @@ export class LoginComponent {
   isRegister: boolean = true;
 
   constructor(
-    private authService: AuthenticationService,
+    private authService: AuthService,
     private router: Router,
     private toastr: ToastrService
   ) {}
 
-  register(registerDto: Register) {
-    this.authService.register(registerDto).
-    pipe(
-      catchError((error) => {
-        this.toastr.error('Erro ao tentar fazer o cadastro. Certifique-se de colocar o código de segurança correto e tente novamente.');
-        throw error;
-      }), tap(() => {
-        this.toastr.success('Cadastro bem-sucedido!')
-      })
-    ).subscribe();
+  //TODO: refactor
+  // register(registerDto: Register) {
+  //   this.authService.register(registerDto).
+  //   pipe(
+  //     catchError((error) => {
+  //       this.toastr.error('Erro ao tentar fazer o cadastro. Certifique-se de colocar o código de segurança correto e tente novamente.');
+  //       throw error;
+  //     }), tap(() => {
+  //       this.toastr.success('Cadastro bem-sucedido!')
+  //     })
+  //   ).subscribe();
+  //
+  //   this.router.navigate(['/home'])
+  //
+  //   this.registerDto = new Register();
+  //
+  // }
 
-    this.router.navigate(['/home'])
-
-    this.registerDto = new Register();
-
-  }
-
-  login(loginDto: Login) {
-    this.authService.login(loginDto).
-    pipe(
-      catchError((error) => {
-        this.toastr.error('Erro ao tentar fazer o login. Verifique o usuário/senha e tente novamente.');
-        throw error;
-      }), tap(() => {
-        this.toastr.success('Login bem-sucedido!')
-      })
-      
-    )
-    .subscribe((jwtDto => {
-      localStorage.setItem('jwtToken', jwtDto.token);
-      
-      this.loginDto = new Login();
-      
-      const token = localStorage.getItem(jwtDto.token);
-      this.authService.authenticate(loginDto);  
-    }));
-  }
+  //TODO: refactor
+  // login(loginDto: Login) {
+  //   this.authService.login(loginDto).
+  //   pipe(
+  //     catchError((error) => {
+  //       this.toastr.error('Erro ao tentar fazer o login. Verifique o usuário/senha e tente novamente.');
+  //       throw error;
+  //     }), tap(() => {
+  //       this.toastr.success('Login bem-sucedido!')
+  //     })
+  //
+  //   )
+  //   .subscribe((jwtDto => {
+  //     localStorage.setItem('jwtToken', jwtDto.token);
+  //
+  //     this.loginDto = new Login();
+  //
+  //     const token = localStorage.getItem(jwtDto.token);
+  //     this.authService.authenticate(loginDto);
+  //   }));
+  // }
 
   toggleForm() {
     this.isLogin = !this.isLogin;
--- src/app/pages/users/users.component.ts
+++ src/app/pages/users/users.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
 import { User } from 'src/app/models/User';
-import { AuthenticationService } from 'src/app/services/authentication.service';
+import { AuthService } from 'src/app/services/auth.service';
 
 
 @Component({
@@ -15,26 +15,27 @@ export class UsersComponent implements OnInit {
   displayedColumns: string[] = ['id', 'username', 'password', 'role', 'acoes'];
   @ViewChild(MatPaginator, { static: true }) paginator: MatPaginator;
 
-  constructor(private authService: AuthenticationService) { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit() {
-    this.authService.getUsers().subscribe(data => {
-      this.dataSource.data = data;
-      this.paginator._intl.itemsPerPageLabel = 'Itens por página';
-      this.paginator._intl.nextPageLabel = 'Próxima';
-      this.paginator._intl.previousPageLabel = 'Anterior';
-      this.paginator._intl.firstPageLabel = 'Primeira página';
-      this.paginator._intl.lastPageLabel = 'Última página';
-      this.paginator._intl.getRangeLabel = (page: number, pageSize: number, length: number) => {
-        if (length == 0 || pageSize == 0) {
-          return `0 de ${length}`;
-        }
-        length = Math.max(length, 0);
-        const startIndex = page * pageSize;
-        const endIndex = startIndex < length ? Math.min(startIndex + pageSize, length) : startIndex + pageSize;
-        return `${startIndex + 1} - ${endIndex} de ${length}`;
-      };
-    });
+    //TODO: refactor
+    // this.authService.getUsers().subscribe(data => {
+    //   this.dataSource.data = data;
+    //   this.paginator._intl.itemsPerPageLabel = 'Itens por página';
+    //   this.paginator._intl.nextPageLabel = 'Próxima';
+    //   this.paginator._intl.previousPageLabel = 'Anterior';
+    //   this.paginator._intl.firstPageLabel = 'Primeira página';
+    //   this.paginator._intl.lastPageLabel = 'Última página';
+    //   this.paginator._intl.getRangeLabel = (page: number, pageSize: number, length: number) => {
+    //     if (length == 0 || pageSize == 0) {
+    //       return `0 de ${length}`;
+    //     }
+    //     length = Math.max(length, 0);
+    //     const startIndex = page * pageSize;
+    //     const endIndex = startIndex < length ? Math.min(startIndex + pageSize, length) : startIndex + pageSize;
+    //     return `${startIndex + 1} - ${endIndex} de ${length}`;
+    //   };
+    // });
   }
 
   addNewUser() {
