027d5e48d24d120d5bb8da22482a525cdf9ef149
celsotavares <celsotavares@pm.me>
2023-11-17 09:09:24 -0300

    refactor: pet service

--- src/app/services/pet.service.ts
+++ src/app/services/pet.service.ts
@@ -2,34 +2,41 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Subject } from 'rxjs';
-import { map } from 'rxjs/operators';
 import { Pet } from '../models/Pet';
 import { PetModel } from '../models/PetModel';
+import { environment } from 'src/environments/environment';
+
+const PETS_API_URL = `${environment.API_URL}/pets`;
 
 @Injectable({
   providedIn: 'root'
 })
 export class PetService {
-  private apiUrl: string = 'http://localhost:8080/pets';
 
   constructor(private http: HttpClient) { }
 
-  getPets(): Observable<Pet[]> {
-    return this.http
-        .get(this.apiUrl)
-        .pipe<Pet[]>(map((data: any) => data));
+  getPets(): Observable<PetModel[]> {
+    return this.http.get<PetModel[]>(PETS_API_URL);
+  }
+
+  getAvailablePets(): Observable<PetModel[]> {
+    return this.http.get<PetModel[]>(`${PETS_API_URL}?status=available`);
+  }
+
+  getUnavailablePets(): Observable<PetModel[]> {
+    return this.http.get<PetModel[]>(`${PETS_API_URL}?status=unavailable`);
   }
 
   addPet(pet: Pet): Observable<Pet> {
-    return this.http.post<Pet>(this.apiUrl, pet);
+    return this.http.post<Pet>(PETS_API_URL, pet);
   }
 
   updatePet(pet: Pet): Observable<Pet> {
-    return this.http.put<Pet>(`${this.apiUrl}/${pet.id}`, pet);
+    return this.http.put<Pet>(`${PETS_API_URL}/${pet.id}`, pet);
   }
 
   deletePet(id: number): Observable<Pet> {
-    return this.http.delete<Pet>(`${this.apiUrl}/${id}`);
+    return this.http.delete<Pet>(`${PETS_API_URL}/${id}`);
   }
 
   private _listners = new Subject<any>();
@@ -38,19 +45,11 @@ export class PetService {
   }
 
   turnAvailable(id: number): Observable<Pet> {
-    return this.http.put<Pet>(`${this.apiUrl}/${id}/available`, {});
-  }  
-
-  getUnavailablePets(): Observable<PetModel[]> {
-    return this.http.get<PetModel[]>(`${this.apiUrl}/unavailable`);
+    return this.http.put<Pet>(`${PETS_API_URL}/${id}/available`, {});
   }
 
   turnUnavailable(id: number): Observable<Pet> {
-    return this.http.delete<Pet>(`${this.apiUrl}/${id}/available`, {});
+    return this.http.delete<Pet>(`${PETS_API_URL}/${id}/available`, {});
   }
 
-  getAllPets(): Observable<PetModel[]> {
-    return this.http.get<PetModel[]>(`${this.apiUrl}/all`);
-  }
-  
 }
