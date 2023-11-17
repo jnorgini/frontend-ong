9dccf32eb2f2b75ce615f6878a94d1828ceb2891
celsotavares <celsotavares@pm.me>
2023-11-17 10:38:58 -0300

    refactor: age-format.pipe

--- src/app/age-format.pipe.ts
+++ src/app/age-format.pipe.ts
@@ -1,13 +1,15 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
+const MONTHS_IN_A_YEAR = 12;
+
 @Pipe({
   name: 'ageFormat'
 })
 export class AgeFormatPipe implements PipeTransform {
   transform(ageInMonths: number): string {
 
-    return ageInMonths >= 12
-      ? `${Math.floor(ageInMonths / 12)} ano(s)`
+    return ageInMonths >= MONTHS_IN_A_YEAR
+      ? `${Math.floor(ageInMonths / MONTHS_IN_A_YEAR)} ano(s)`
       : `${ageInMonths} mês(es)`;
   }
 }
