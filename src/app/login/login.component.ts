1acfa7c028e0f0d3c21fdec926c6b591b0e60be2
celsotavares <celsotavares@pm.me>
2023-11-17 08:57:57 -0300

    refactor: login component

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,76 +1,48 @@
-import { Component } from '@angular/core';
-import { Login } from '../models/login';
-import { Register } from '../models/register';
-import { JwtAuth } from '../models/jwtAuth';
-import { AuthService } from '../services/auth.service';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { catchError, tap, throwError } from 'rxjs';
+import { catchError, tap } from 'rxjs';
 import { ToastrService } from 'ngx-toastr';
+import { Login } from '../models/login';
+import { AuthService } from '../services/auth.service';
+import { TokenStorageService } from '../services/token-storage.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
-  loginDto = new Login();
-  registerDto = new Register();
-  jwtDto = new JwtAuth();
-  isLogin: boolean = true;
-  isRegister: boolean = true;
+export class LoginComponent implements OnInit {
+  loginDto: Login = new Login();
 
   constructor(
     private authService: AuthService,
+    private toastr: ToastrService,
     private router: Router,
-    private toastr: ToastrService
+    private tokenStorage: TokenStorageService
   ) {}
 
-  //TODO: refactor
-  // register(registerDto: Register) {
-  //   this.authService.register(registerDto).
-  //   pipe(
-  //     catchError((error) => {
-  //       this.toastr.error('Erro ao tentar fazer o cadastro. Certifique-se de colocar o código de segurança correto e tente novamente.');
-  //       throw error;
-  //     }), tap(() => {
-  //       this.toastr.success('Cadastro bem-sucedido!')
-  //     })
-  //   ).subscribe();
-  //
-  //   this.router.navigate(['/home'])
-  //
-  //   this.registerDto = new Register();
-  //
-  // }
-
-  //TODO: refactor
-  // login(loginDto: Login) {
-  //   this.authService.login(loginDto).
-  //   pipe(
-  //     catchError((error) => {
-  //       this.toastr.error('Erro ao tentar fazer o login. Verifique o usuário/senha e tente novamente.');
-  //       throw error;
-  //     }), tap(() => {
-  //       this.toastr.success('Login bem-sucedido!')
-  //     })
-  //
-  //   )
-  //   .subscribe((jwtDto => {
-  //     localStorage.setItem('jwtToken', jwtDto.token);
-  //
-  //     this.loginDto = new Login();
-  //
-  //     const token = localStorage.getItem(jwtDto.token);
-  //     this.authService.authenticate(loginDto);
-  //   }));
-  // }
-
-  toggleForm() {
-    this.isLogin = !this.isLogin;
+  ngOnInit(): void {
+    if (this.tokenStorage.getAccessToken()) {
+      this.router.navigate(['/home']).then(() => {});
+    }
   }
 
-  showToaster() {
-    this.toastr.success('Some messages', 'title');
+  login(loginDto: Login) {
+    this.authService.login(loginDto)
+      .pipe(
+        catchError((error) => {
+          this.toastr.error('Erro ao tentar fazer o login. Verifique o usuário/senha e tente novamente.');
+          throw error;
+        }),
+        tap(() => {
+          this.toastr.success('Login bem-sucedido!')
+        })
+      )
+      .subscribe((jwtDto => {
+        this.tokenStorage.saveAccessToken(jwtDto.accessToken);
+        this.tokenStorage.saveRefreshToken(jwtDto.refreshToken);
+        window.location.reload();
+      }));
   }
 
 }
